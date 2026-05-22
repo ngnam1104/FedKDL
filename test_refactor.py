160452864e7a825e5690c43d5e8802911595164f
ngnam1104 <nam.nghai24@gmail.com>
2026-05-23 04:22:39 +0700

    Remove test_refactor.py

--- test_refactor.py
+++
@@ -1,4 +0,0 @@
-from federated_core.base_simulator import BaseSimulator
-from tasks.anomaly_1d.simulator import Simulator1D
-from tasks.detection_2d.simulator import Simulator2D
-print("Import thành công!")
